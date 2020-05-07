e8ec9193d1f47febe4659719a98580ecff88b8c7
Dennie Froese <dennie.froese@santander.co.uk>
2020-05-07 11:53:28 +0100

    added push functionality to LogonRouter, pushing Binding

--- src/navigation/LogonRouter/index.tsx
+++ src/navigation/LogonRouter/index.tsx
@@ -1,7 +1,24 @@
 import React from 'react';
 import EasyRouter from 'react-native-easy-router';
 import Logon from 'pages/Logon';
+import Binding from 'pages/Binding';
+
+let router: any;
 
 export default function LogonRouter() {
-  return <EasyRouter initialStack="Logon" screens={{Logon}} />;
+  return (
+    <EasyRouter
+      navigatorRef={setRouter}
+      initialStack="Logon"
+      screens={{Logon, Binding}}
+    />
+  );
+}
+
+export function pushBinding() {
+  router.push('Binding');
+}
+
+function setRouter(r: any) {
+  router = r;
 }
